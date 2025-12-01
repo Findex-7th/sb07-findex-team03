06b661cadab4cd0594b1a631a06447a74f82606b
hyo37009 <hyo37009@gmail.com>
2025-12-01 18:05:57 +0900

    feat: OpenApiService 클래스 추가 및 외부 API 호출 로직 구현

---
+++ src/main/java/com/team3/findex/service/OpenApiService.java
@@ -0,0 +1,55 @@
+package com.team3.findex.service;
+
+import com.team3.findex.openapi.IndexApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@RequiredArgsConstructor
+public class OpenApiService {
+
+    @Value("${open-api.key}")
+    private String serviceKey;
+
+    @Value("${open-api.url}")
+    private String url;
+
+    private final RestTemplate restTemplate;
+
+    public String getOpenApiDataByDate(LocalDate date) {
+//        Map<String, Object> resultMap = new HashMap<String, Object>();
+//
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
+//
+        UriComponents uri = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("resultType", "json")
+                .queryParam("serviceKey", serviceKey)
+                .queryParam("basDt", date.format(DateTimeFormatter.ofPattern("yyyyMMdd")))
+                .build();
+
+        return restTemplate.getForObject(uri.toUriString(), String.class);
+    }
+    public IndexApiResponse getOpenApiDataByDate2(LocalDate date) {
+//        Map<String, Object> resultMap = new HashMap<String, Object>();
+//
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
+//
+        UriComponents uri = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("resultType", "json")
+                .queryParam("serviceKey", serviceKey)
+                .queryParam("basDt", date.format(DateTimeFormatter.ofPattern("yyyyMMdd")))
+                .build();
+
+        return restTemplate.getForObject(uri.toUriString(), IndexApiResponse.class);
+    }
+
+}
