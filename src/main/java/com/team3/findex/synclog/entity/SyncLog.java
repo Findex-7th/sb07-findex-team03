d9a5206e42e0500792253db55a72ce9b4126b134
JoSungYeon-000607 <zounsam00@gmail.com>
2025-12-01 15:33:02 +0900

    feat : 연동 이력 엔티디
    ***
    
    ***

---
+++ src/main/java/com/team3/findex/synclog/entity/SyncLog.java
@@ -0,0 +1,19 @@
+package com.team3.findex.synclog.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+public class SyncLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+}
