67bfeb806d06eb19106c5bd3d85261a98fc314bc
dogyeom <sprfv@naver.com>
2025-12-01 18:45:21 +0900

    feat: IndexInfo 도메인 엔티티 생성
    - Swagger 명세 기준으로 필드 설계
    - SourceType enum 추가
    - 상태 변경용 update 메서드 포함

---
+++ src/main/java/com/team3/findex/domain/indexInfo/entity/IndexInfo.java
@@ -0,0 +1,62 @@
+package com.team3.findex.domain.indexInfo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "index_info")
+public class IndexInfo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 150)
+  private String indexClassification;
+
+  @Column(nullable = false, length = 150)
+  private String indexName;
+
+  @Column(nullable = false)
+  private Integer employedItemsCount;
+
+  @Column(nullable = false)
+  private LocalDate basePointInTime;
+
+  @Column(nullable = false)
+  private Double baseIndex;
+
+  @Column(nullable = false)
+  private Boolean favorite = false;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, length = 20)
+  private SourceType sourceType;
+
+
+  public void update(
+      Integer employedItemsCount,
+      LocalDate basePointInTime,
+      Double baseIndex,
+      Boolean favorite
+  ) {
+    if(employedItemsCount != null) { this.employedItemsCount = employedItemsCount; }
+    if(basePointInTime != null) { this.basePointInTime = basePointInTime; }
+    if(baseIndex != null) { this.baseIndex = baseIndex; }
+    if(favorite != null && !favorite.equals(this.favorite)) { this.favorite = favorite; }
+  }
+}
