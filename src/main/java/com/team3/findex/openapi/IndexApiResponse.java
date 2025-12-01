56217f707c7b29b424b145271693d367399fb3af
hyo37009 <hyo37009@gmail.com>
2025-12-01 18:18:49 +0900

    refactor: IndexApiResponse 클래스에 JsonIgnoreProperties 추가 및 불필요한 필드 무시 처리

--- src/main/java/com/team3/findex/openapi/IndexApiResponse.java
+++ src/main/java/com/team3/findex/openapi/IndexApiResponse.java
@@ -1,5 +1,7 @@
 package com.team3.findex.openapi;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
@@ -10,6 +12,7 @@ import lombok.Getter;
  * </p>
  */
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class IndexApiResponse {
 
   @JsonProperty("basPntm")
