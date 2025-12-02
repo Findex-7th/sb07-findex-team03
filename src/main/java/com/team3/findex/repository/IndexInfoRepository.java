580e369b65f9735fc51502f0836266ee4e7c35e1
hyo37009 <hyo37009@gmail.com>
2025-12-02 09:48:32 +0900

    feat: JPA Repository 인터페이스 추가

---
+++ src/main/java/com/team3/findex/repository/IndexInfoRepository.java
@@ -0,0 +1,6 @@
+package com.team3.findex.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndexInfoRepository extends JpaRepository<IndexInfoRepository, Long> {
+}
