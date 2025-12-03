14b96007cfbc97cec53a80bff1eb7eec6519a104
JoSungYeon-000607 <zounsam00@gmail.com>
2025-12-03 18:07:27 +0900

    feat : 중간 점검

--- src/main/java/com/team3/findex/domain/syncjob/service/SyncJobService.java
+++ src/main/java/com/team3/findex/domain/syncjob/service/SyncJobService.java
@@ -28,6 +28,17 @@ public class SyncJobService {
     private final SyncJobMapper syncJobMapper;
 
 
+    /**
+     * 전체 지수 정보(Index Info)에 대한 동기화 작업을 수행하고 이력을 저장합니다.
+     * <p>
+     * 등록된 모든 지수 정보를 조회하여 순차적으로 연동 작업을 시도합니다.
+     * 특정 지수 정보 처리 중 오류가 발생하거나 유효성 검증에 실패할 경우,
+     * 전체 프로세스를 중단하지 않고 해당 건에 대해 '실패' 로그를 저장한 후 다음 작업을 계속 진행합니다.
+     * </p>
+     *
+     * @param worker 작업을 수행하는 주체 (사용자의 Ip주소)
+     * @return 수행된 모든 연동 작업의 결과 로그(SyncJobDto) 리스트 (성공 및 실패 포함)
+     */
     @Transactional
     public List<SyncJobDto> syncIndexInfos(String worker){
         List<IndexInfo> indexInfos = indexInfoRepository.findAll();
@@ -56,12 +67,31 @@ public class SyncJobService {
         return null;
     }
 
+    /**
+     * [내부 메서드] 작업 성공 상태의 로그를 생성하고 저장합니다.
+     *
+     * @param jobType 작업 유형 (INDEX_INFO: 지수 정보, INDEX_DATA: 지수 데이터)
+     * @param worker 작업 수행자
+     * @param indexInfo 대상 지수 정보 엔티티
+     * @return DB에 저장된 성공 상태(Result.SUCCESS)의 SyncJob 엔티티
+     */
     @Transactional
     protected SyncJob createSuccessLog(JobType jobType, String worker, IndexInfo indexInfo) {
         SyncJob successJob = SyncJob.ofSuccess(jobType, worker, indexInfo);
          return syncJobRepository.save(successJob);
     }
 
+    /**
+     * [내부 메서드] 작업 실패 상태의 로그를 생성하고 저장합니다.
+     * <p>
+     * 예외 발생 시 호출되며, 트랜잭션 롤백 없이 실패 이력을 남기기 위해 사용됩니다.
+     * </p>
+     *
+     * @param jobType 작업 유형 (INDEX_INFO: 지수 정보, INDEX_DATA: 지수 데이터)
+     * @param worker 작업 수행자
+     * @param indexInfo 대상 지수 정보 엔티티
+     * @return DB에 저장된 실패 상태(Result.FAILED)의 SyncJob 엔티티
+     */
     @Transactional
     protected SyncJob createFailureLog(JobType jobType, String worker, IndexInfo indexInfo) {
         SyncJob failJob = SyncJob.ofFailure(jobType, worker, indexInfo);
