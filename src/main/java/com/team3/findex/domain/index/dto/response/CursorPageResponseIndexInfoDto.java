243782149e53a6487d50384c50524f30d1815279
hyo37009 <hyo37009@gmail.com>
2025-12-07 20:19:13 +0900

    fix: CursorPageResponseIndexInfoDto 필드명 오타 수정
    - `newtIdAfter`를 `nextIdAfter`로 수정하여 필드명 오류 해결.

--- src/main/java/com/team3/findex/domain/index/dto/response/CursorPageResponseIndexInfoDto.java
+++ src/main/java/com/team3/findex/domain/index/dto/response/CursorPageResponseIndexInfoDto.java
@@ -5,7 +5,7 @@ import java.util.List;
 public record CursorPageResponseIndexInfoDto(
     List<IndexInfoDto> content,
     String nextCursor,
-    String newtIdAfter,
+    String nextIdAfter,
     int size,
     Long totalElements,
     boolean hasNext
