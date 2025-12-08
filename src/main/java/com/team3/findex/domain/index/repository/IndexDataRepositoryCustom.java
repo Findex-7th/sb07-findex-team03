485277eff461343e8cc473adffbdfbd81202bb1f
hyo37009 <hyo37009@gmail.com>
2025-12-08 11:40:54 +0900

    refactor: 불필요한 import문 정리 및 wildcard import로 변경
    - `IndexDataRepositoryCustom`에서 사용되지 않는 import 제거.
    - `import com.team3.findex.domain.index.dto.*`로 변경하여 가독성 개선.

--- src/main/java/com/team3/findex/domain/index/repository/IndexDataRepositoryCustom.java
+++ src/main/java/com/team3/findex/domain/index/repository/IndexDataRepositoryCustom.java
@@ -2,10 +2,7 @@ package com.team3.findex.domain.index.repository;
 
 import com.team3.findex.domain.index.IndexData;
 import com.team3.findex.domain.index.IndexInfo;
-import com.team3.findex.domain.index.dto.IndexDataFindCondition;
-import com.team3.findex.domain.index.dto.IndexDataFindSort;
-import com.team3.findex.domain.index.dto.IndexInfoFindCondition;
-import com.team3.findex.domain.index.dto.IndexInfoFindSort;
+import com.team3.findex.domain.index.dto.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
